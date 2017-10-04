70edfa89033fe5ea3dd0f95ea54531c9b68a906f
FedorovaVika <32073057+FedorovaVika@users.noreply.github.com>
2017-10-04 19:33:26 +0300

    Add files via upload

---
+++ z21.m
@@ -0,0 +1,14 @@
+fs = 1000;
+bp = [-3 12 17 12 -3]/35; 
+ap = 1;
+n = 512;
+figure(21);
+[h,w] = freqz(bp,ap,n);
+mag = abs(h); 
+phase = angle(h)*180/pi;
+subplot(2, 1, 1), plot(w/(2*pi)*fs,mag), grid on;
+xlabel ('fs'); 
+title ('юву');
+subplot(2, 1, 2), plot(w/(2*pi)*fs,unwrap(phase)), grid on;
+xlabel ('fs'); 
+title ('тву');
